51f1b62e3b76612a5eddc8b061c8db467838070e
LuckyKing0307 <jasaergasheva@gmail.com>
2025-07-14 10:01:32 +0500

    ParcerJob.php

--- resources/views/layouts/checkout.blade.php
+++ resources/views/layouts/checkout.blade.php
@@ -7,8 +7,8 @@
         name="viewport"
         content="width=device-width, initial-scale=1"
     >
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>Demo Storefront</title>
+    <title>@yield('title', config('app.name'))</title>
+    <meta name="title" content="@yield('title', config('app.name'))">
     <meta
         name="description"
         content="Example of an ecommerce storefront built with Lunar."
@@ -18,37 +18,50 @@
         rel="stylesheet"
     >
     <link
+        href="{{ asset('css/main.css') }}"
+        rel="stylesheet"
+    >
+    @stack('page-styles')
+    <link
         rel="icon"
         href="{{ asset('icon.svg') }}"
     >
     @livewireStyles
-    @stripeScripts
     @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/noUiSlider/15.7.0/nouislider.min.css" rel="stylesheet"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.css" />
+    <link rel="stylesheet"
+          href="https://unpkg.com/beerslider/dist/BeerSlider.css">
+    <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+    <script src="https://api-maps.yandex.ru/2.1/?apikey=ad99ba7e-6736-4900-9db3-b2f246c8b1a8&lang=ru_RU" type="text/javascript"></script>
 </head>
 
 <body class="antialiased text-gray-900">
-    <header class="relative border-b border-gray-100">
-        <div class="flex items-center h-16 px-4 mx-auto max-w-screen-2xl sm:px-6 lg:px-8">
-            <a
-                class="flex items-center flex-shrink-0"
-                href="{{ url('/') }}"
-            >
-                <span class="sr-only">Home</span>
+@livewire('components.navigation')
 
-                <x-brand.logo class="w-auto h-6 text-indigo-600" />
-            </a>
-        </div>
-    </header>
+<main>
+    {{ $slot }}
+</main>
 
+<x-footer/>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.js"></script>
+<script>
+    AOS.init();
+</script>
+@livewireScripts
+<script>(function(a,m,o,c,r,m){a[m]={id:"430467",hash:"7462463ff36f16ac96d963c482521d04374a712c44e99cbb71f0e91dd62bdcd3",locale:"ru",inline:false,setMeta:function(p){this.params=(this.params||[]).concat([p])}};a[o]=a[o]||function(){(a[o].q=a[o].q||[]).push(arguments)};var d=a.document,s=d.createElement('script');s.async=true;s.id=m+'_script';s.src='https://gso.amocrm.ru/js/button.js';d.head&&d.head.appendChild(s)}(window,0,'amoSocialButton',0,0,'amo_social_button'));</script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/noUiSlider/15.7.0/nouislider.min.js"></script>
 
-    <main>
-        {{ $slot }}
-    </main>
-
-    <x-footer />
-
-    @livewireScripts
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/noUiSlider/15.7.0/nouislider.min.js"></script>
+<script defer>
+    document.addEventListener('DOMContentLoaded', function() {
+        setInterval(function(){
+            amoSocialButton('onChatReady', function () {
+                amoSocialButton('runChatShow');
+            });
+        }, 100); // небольшая задержка для надёжности
+    });
+</script>
 </body>
 
 </html>
