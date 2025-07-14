2389ba1e73dc8b0a90aa1da12660c2d57cef3472
LuckyKing0307 <jasaergasheva@gmail.com>
2025-07-14 10:30:46 +0500

    ParcerJob.php

--- app/Livewire/CheckoutPage.php
+++ app/Livewire/CheckoutPage.php
@@ -12,75 +12,45 @@ use Lunar\Facades\ShippingManifest;
 use Lunar\Models\Cart;
 use Lunar\Models\CartAddress;
 use Lunar\Models\Country;
-use Telegram\Bot\Laravel\Facades\Telegram;
 
 class CheckoutPage extends Component
 {
-    /**
-     * The Cart instance.
-     */
     public ?Cart $cart;
-
-    /**
-     * The shipping address instance.
-     */
     public ?CartAddress $shipping = null;
 
-    /**
-     * The billing address instance.
-     */
-    public ?CartAddress $billing = null;
-
-    /**
-     * The current checkout step.
-     */
     public int $currentStep = 1;
-
-    /**
-     * Whether the shipping address is the billing address too.
-     */
     public bool $shippingIsBilling = true;
-
-    /**
-     * The chosen shipping option.
-     */
     public $chosenShipping = null;
 
-    /**
-     * The checkout steps.
-     */
     public array $steps = [
         'shipping_address' => 1,
     ];
 
-    /**
-     * The payment type we want to use.
-     */
     public string $paymentType = 'cash-in-hand';
-
-    /**
-     * {@inheritDoc}
-     */
-    protected $listeners = [
-        'cartUpdated' => 'refreshCart',
-        'selectedShippingOption' => 'refreshCart',
-    ];
+    public array $rules = [];
 
     public $payment_intent = null;
-
     public $payment_intent_client_secret = null;
 
     protected $queryString = [
         'payment_intent',
         'payment_intent_client_secret',
     ];
-    public array $rules = [];
-    /**
-     * {@inheritDoc}
-     */
+
+    protected $listeners = [
+        'cartUpdated' => 'refreshCart',
+        'selectedShippingOption' => 'refreshCart',
+    ];
 
     public function mount(): void
     {
+        if (! $this->cart = CartSession::current()) {
+            $this->redirect('/');
+            return;
+        }
+
+        $this->shipping = $this->cart->shippingAddress ?: new CartAddress;
+
         $this->rules = array_merge(
             $this->getAddressValidation('shipping'),
             [
@@ -88,29 +58,6 @@ class CheckoutPage extends Component
                 'chosenShipping' => 'required',
             ]
         );
-        if (! $this->cart = CartSession::current()) {
-            $this->redirect('/');
-
-            return;
-        }
-
-        if ($this->payment_intent) {
-            $payment = Payments::driver($this->paymentType)->cart($this->cart)->withData([
-                'payment_intent_client_secret' => $this->payment_intent_client_secret,
-                'payment_intent' => $this->payment_intent,
-            ])->authorize();
-
-            if ($payment->success) {
-                redirect()->route('checkout-success.view');
-
-                return;
-            }
-        }
-
-        // Do we have a shipping address?
-
-        $this->shipping = $this->cart->shippingAddress ?: new CartAddress;
-        $this->billing = clone $this->shipping;
 
         $this->determineCheckoutStep();
     }
@@ -120,114 +67,72 @@ class CheckoutPage extends Component
         $this->cart = CartSession::current();
     }
 
-    /**
-     * Trigger an event to refresh addresses.
-     */
-    public function triggerAddressRefresh(): void
-    {
-        $this->dispatch('refreshAddress');
-    }
-
-    /**
-     * Determines what checkout step we should be at.
-     */
     public function determineCheckoutStep(): void
     {
         $shippingAddress = $this->cart->shippingAddress;
-        $billingAddress = $this->cart->billingAddress;
 
-        if ($shippingAddress) {
-            if ($shippingAddress->id) {
-                $this->currentStep = $this->steps['shipping_address'] + 1;
-            }
+        if ($shippingAddress && $shippingAddress->id) {
+            $this->currentStep = $this->steps['shipping_address'] + 1;
 
-            // Do we have a selected option?
             if ($this->shippingOption) {
                 $this->chosenShipping = $this->shippingOption->getIdentifier();
-                $this->currentStep = $this->steps['shipping_option'] + 1;
+                $this->currentStep = $this->steps['shipping_address'] + 2;
             } else {
-                $this->currentStep = $this->steps['shipping_option'];
                 $this->chosenShipping = $this->shippingOptions->first()?->getIdentifier();
-
                 return;
             }
         }
-
-        if ($billingAddress) {
-            $this->currentStep = $this->steps['billing_address'] + 1;
-        }
     }
 
-    /**
-     * Refresh the cart instance.
-     */
     public function refreshCart(): void
     {
         $this->cart = CartSession::current();
     }
 
-    /**
-     * Return the shipping option.
-     */
     public function getShippingOptionProperty()
     {
         $shippingAddress = $this->cart->shippingAddress;
 
         if (! $shippingAddress) {
-            return;
+            return null;
         }
 
         if ($option = $shippingAddress->shipping_option) {
-            return ShippingManifest::getOptions($this->cart)->first(function ($opt) use ($option) {
-                return $opt->getIdentifier() == $option;
-            });
+            return ShippingManifest::getOptions($this->cart)->first(fn ($opt) => $opt->getIdentifier() == $option);
         }
 
         return null;
     }
 
-    /**
-     * Save the address for a given type.
-     */
+    public function getShippingOptionsProperty(): Collection
+    {
+        return ShippingManifest::getOptions($this->cart);
+    }
+
+    public function getCountriesProperty(): Collection
+    {
+        return Country::whereIn('iso3', ['UZB'])->get();
+    }
+
     public function saveAddress(string $type): void
     {
-        $validatedData = $this->validate(
-            $this->getAddressValidation($type)
-        );
+        $validatedData = $this->validate($this->getAddressValidation($type));
 
         $address = $this->{$type};
 
-        if ($type == 'billing') {
-            $this->cart->setBillingAddress($address);
-            $this->billing = $this->cart->billingAddress;
-        }
-
-        if ($type == 'shipping') {
+        if ($type === 'shipping') {
             $this->cart->setShippingAddress($address);
+
+            // ВАЖНО: всегда дублируем shipping в billing
+            $billingData = $address->only($address->getFillable());
+            $this->cart->setBillingAddress($billingData);
+
             $this->shipping = $this->cart->shippingAddress;
-
-            if ($this->shippingIsBilling) {
-                // Do we already have a billing address?
-                if ($billing = $this->cart->billingAddress) {
-                    $billing->fill($validatedData['shipping']);
-                    $this->cart->setBillingAddress($billing);
-                } else {
-                    $address = $address->only(
-                        $address->getFillable()
-                    );
-                    $this->cart->setBillingAddress($address);
-                }
-
-                $this->billing = $this->cart->billingAddress;
-            }
         }
 
         $this->determineCheckoutStep();
     }
 
-    /**
-     * Save the selected shipping option.
-     */
     public function saveShippingOption(): void
     {
         $option = $this->shippingOptions->first(fn ($option) => $option->getIdentifier() == $this->chosenShipping);
@@ -235,63 +140,38 @@ class CheckoutPage extends Component
         CartSession::setShippingOption($option);
 
         $this->refreshCart();
-
         $this->determineCheckoutStep();
     }
 
     public function checkout()
     {
+        // Защита от отсутствия billing
+        if (!$this->cart->billingAddress) {
+            $billingData = $this->shipping->only($this->shipping->getFillable());
+            $this->cart->setBillingAddress($billingData);
+        }
+
         $payment = Payments::cart($this->cart)->withData([
             'payment_intent_client_secret' => $this->payment_intent_client_secret,
             'payment_intent' => $this->payment_intent,
         ])->authorize();
 
         if ($payment->success) {
-            redirect()->route('checkout-success.view');
             $lines = $this->cart->lines->map(function($line) {
                 return "{$line->quantity}× «{$line->purchasable->getDescription()}» — {$line->subTotal->formatted()}";
             })->implode("\n");
 
             $shipping = $this->cart->shippingAddress;
-            $billing  = $this->cart->billingAddress;
-
-//            $text = "<b>🛒 Новый заказ #{$this->cart->id}</b>\n"
-//                . "Сумма: {$this->cart->total->formatted()}\n\n"
-//                . "<u>Товары:</u>\n{$lines}\n\n"
-//                . "<u>Доставка:</u>\n"
-//                . "{$shipping->first_name} {$shipping->last_name}, "
-//                . "{$shipping->line_one} {$shipping->city}, {$shipping->postcode}\n\n"
-//                . "<u>Оплата:</u> {$this->paymentType}\n";
 
             $amo = new AmoController();
-            $amo->createLead('Заказ с сайта от - '.$shipping->first_name,$this->cart->total->value);
+            $amo->createLead('Заказ с сайта от - ' . $shipping->first_name, $this->cart->total->value);
 
+            return redirect()->route('checkout-success.view');
         }
 
         return redirect()->route('checkout-success.view');
     }
 
-    /**
-     * Return the available countries.
-     */
-    public function getCountriesProperty(): Collection
-    {
-        return Country::whereIn('iso3', ['UZB'])->get();
-    }
-
-    /**
-     * Return available shipping options.
-     */
-    public function getShippingOptionsProperty(): Collection
-    {
-        return ShippingManifest::getOptions(
-            $this->cart
-        );
-    }
-
-    /**
-     * Return the address validation rules for a given type.
-     */
     protected function getAddressValidation(string $type): array
     {
         return [
@@ -303,7 +183,6 @@ class CheckoutPage extends Component
 
     public function render(): View
     {
-        return view('livewire.checkout-page')
-            ->layout('layouts.checkout');
+        return view('livewire.checkout-page')->layout('layouts.checkout');
     }
 }
