9ccf6889f5e4484ab7bc32d11c52a6736e97df80
LuckyKing0307 <jasaergasheva@gmail.com>
2025-07-16 11:45:51 +0500

    Update

--- resources/views/components/collection-sale.blade.php
+++ resources/views/components/collection-sale.blade.php
@@ -3,7 +3,7 @@
         <div class="content">
             <h1 class="title">Напольные решения для вашего интерьера</h1>
             <p class="text">Более 500 вариантов ламината и паркета от ведущих брендов</p>
-            <a href="" class="btn btn-main">Перейти в каталог</a>
+            <a href="{{route('collection.view.all')}}" class="btn btn-main">Перейти в каталог</a>
         </div>
         <div class="header_img">
             <img src="{{asset('img/header-bg.svg')}}" alt="">
--- resources/views/livewire/pages/basket.blade.php
+++ resources/views/livewire/pages/basket.blade.php
@@ -6,7 +6,7 @@
         <div class="container">
             <img src="{{ asset('img/cart.png') }}" class="basket__img" alt="Корзина" height="211" width="229" />
             <h1 class="basket__title">В корзине пока нет товаров</h1>
-            <a href="#" class="basket__btn">Перейти в каталог</a>
+            <a href="{{route('collection.view.all')}}" class="basket__btn">Перейти в каталог</a>
             <p class="basket__subtitle">
                 Индивидуальный подбор товара по телефону
                 <a href="tel:+998901234567" class="basket__phone">998 90 123 45 67</a>
