7363fa9af06f8f7b90b54772b734cdd1e68bec6a
LuckyKing0307 <jasaergasheva@gmail.com>
2025-07-16 12:33:43 +0500

    Update

--- app/Livewire/Components/BrandsStrip.php
+++ app/Livewire/Components/BrandsStrip.php
@@ -7,17 +7,14 @@ use Lunar\Models\Brand;
 
 class BrandsStrip extends Component
 {
-    public function getBrandsProperty()
+    public function render()
     {
-        // Можно заменить orderBy() на random() при желании
-        return Brand::with('thumbnail')
+
+        $brands = Brand::with('thumbnail')
             ->orderBy('name')
             ->take(7)
             ->get();
-    }
 
-    public function render()
-    {
         return view('livewire.components.brands-strip', [
             'brands' => $this->brands,
         ]);
