762c67748bf5b58fff379a69bb888bf5a5f59943
LuckyKing0307 <jasaergasheva@gmail.com>
2025-07-16 12:52:51 +0500

    Update

--- resources/views/livewire/collection-page.blade.php
+++ resources/views/livewire/collection-page.blade.php
@@ -2,6 +2,7 @@
 <section>
     @livewire('catalog-novigation')
     @livewire('components.filtration')
+    {{$this->brand}}
     @if (!empty($this->activeFilters))
         <div class="mb-6 flex flex-wrap items-center gap-2">
             @foreach ($this->activeFilters as $filterId)
