e0a603a8b1c61f18e3fd73e96baaf213f54e2e56
LuckyKing0307 <jasaergasheva@gmail.com>
2025-07-16 16:11:52 +0500

    Update

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -5,13 +5,23 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Laravel\Scout\Searchable;
 use Lunar\Models\Product as LunarProduct;
 
 class Product extends LunarProduct
 {
-
+    use Searchable;
     public function characteristics(): HasMany
     {
         return $this->hasMany(ProductCharacteristic::class);
+    }
+    public function toSearchableArray()
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->translateAttribute('name'), // или $this->name если уже в текущей локали
+            'description' => $this->translateAttribute('description'),
+            // любые другие нужные поля
+        ];
     }
 }
