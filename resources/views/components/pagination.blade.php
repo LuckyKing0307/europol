ec74bfc005a49317f75fc19be5d2f744456f55df
LuckyKing0307 <jasaergasheva@gmail.com>
2025-07-17 15:33:04 +0500

    Update

--- resources/views/components/pagination.blade.php
+++ resources/views/components/pagination.blade.php
@@ -17,9 +17,9 @@
         {{-- Left part (first half) --}}
         @foreach ($allPages->slice(0, $half) as $page => $url)
             @if ($page == $current)
-                <span class="rounded-full border p-2 font-bold text-blue-600">{{ $page }}</span>
+                <span class="rounded-full border p-2 font-bold text-blue-600" style="color: white;">{{ $page }}</span>
             @else
-                <button wire:click="gotoPage({{ $page }})" class="p-2 hover:text-blue-500 texted_btn">{{ $page }}</button>
+                <button wire:click="gotoPage({{ $page }})" class="p-2 hover:text-blue-500 texted_btn" style="color: white;">{{ $page }}</button>
             @endif
         @endforeach
 
