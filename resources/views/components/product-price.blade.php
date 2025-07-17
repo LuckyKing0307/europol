7918c21c4c1c91c7f510bf6778ff58901c0c7e88
LuckyKing0307 <jasaergasheva@gmail.com>
2025-07-17 16:20:18 +0500

    Update

--- resources/views/components/product-price.blade.php
+++ resources/views/components/product-price.blade.php
@@ -1,6 +1,6 @@
 <span {{ $attributes }}>
     @if($price && $price->price)
-        {{ number_format($price->price->value * $rate / 100, 2, ',', ' ') }} UZS
+        {{ number_format($price->price->value * $rate / 100, 0, ',', ' ') }} UZS
     @else
         0,00 UZS
     @endif
