4f20de547adf49fd915da211a9e95733eeba7f0b
LuckyKing0307 <jasaergasheva@gmail.com>
2025-07-17 12:00:48 +0500

    Update

--- resources/views/components/product-card.blade.php
+++ resources/views/components/product-card.blade.php
@@ -1,28 +1,4 @@
 @props(['product'])
-@php
-$info = [
-            'klass-iznosostoikosti' => [
-                'img' => 'img/shield.svg',
-                'key' => 'Класс износостойкости:',
-                'value' => $product->translateAttribute('klass-iznosostoikosti')!='' ? $product->translateAttribute('klass-iznosostoikosti') : 0
-            ],
-            'tolshhina-paneli' => [
-                'img' => 'img/tolshina.svg',
-                'key' => 'Толщина панели:',
-                'value' => $product->translateAttribute('tolshhina-paneli') != '' ? $product->translateAttribute('tolshhina-paneli').' мм' : 0
-            ],
-            'faska-po-perimetru' => [
-                'img' => 'img/faska.svg',
-                'key' => 'Фаска по периметру:',
-                'value' => $product->translateAttribute('faska-po-perimetru') != '' ? $product->translateAttribute('faska-po-perimetru') : 'Нету'
-            ],
-            'ploshhad' => [
-                'img' => 'img/ploshhad.svg',
-                'key' => 'Площадь в упаковке:',
-                'value' => $product->translateAttribute('ploshhad') != '' ? $product->translateAttribute('ploshhad').' м²' : 0
-            ],
-        ]
-@endphp
 <div class="swiper-slide product p-4">
     <a class="rounded-lg text-center product_medium_img"
        href="{{ route('product.view', $product->defaultUrl->slug) }}">
@@ -51,9 +27,9 @@ $info = [
         <a class="rounded-lg text-center product_medium_img"
            href="{{ route('product.view', $product->defaultUrl->slug) }}">
         <div class="product_info">
-            @foreach($info  as $product_info)
+            @foreach($product->characteristics()->limit(4)->get()  as $product_info)
                 <p class="product_info_text"><img
-                            src="{{asset($product_info['img'])}}"><span> {{$product_info['key']}} </span>{{$product_info['value']!='' ? $product_info['value']  : 'нет'}}
+                            src=""><span> {{$product_info['key']}} </span>{{$product_info['value']!='' ? $product_info['value']  : 'нет'}}
                 </p>
             @endforeach
             <p class="product_info_text"><img src="{{asset('img/car.svg')}}">Бесплатная доставка по городу</p>
