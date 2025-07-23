0fd94cef384462107c15683d599e9d88c3865c68
LuckyKing0307 <jasaergasheva@gmail.com>
2025-07-23 14:12:01 +0500

    Update

--- resources/views/livewire/checkout-page.blade.php
+++ resources/views/livewire/checkout-page.blade.php
@@ -21,27 +21,25 @@
                                     <div class="mt-1 text-xs text-gray-500">
                                         {{ $line->quantity }} × {{ $line->subTotal->formatted() }}
                                     </div>
-
-                                    <div class="flex items-center mt-2 space-x-2 text-sm">
-                                        <button
-                                            class="px-2 py-1  bg-gray-600 rounded hover:bg-gray-700"
-                                            style="color:#000;"
-                                            wire:click="decreaseQuantity('{{ $line->id }}')"
-                                            title="Уменьшить количество">–</button>
-
-                                        <span>{{ $line->quantity }}</span>
-
-                                        <button
-                                            class="px-2 py-1  bg-gray-600 rounded hover:bg-gray-700"
-                                            style="color:#000;"
-                                            wire:click="increaseQuantity('{{ $line->id }}')"
-                                            title="Увеличить количество">+</button>
-
-                                        <button
-                                            class="px-2 py-1  bg-red-500 rounded hover:bg-red-600"
-                                            style="color:#000;"
-                                            wire:click="removeLine('{{ $line->id }}')"
-                                            title="Удалить товар">✕</button>
+                                    <div class="flex justify-between">
+                                        <div class="flex items-center mt-2 space-x-2 text-sm">
+                                            <button
+                                                class="px-2 py-1  bg-gray-600 rounded hover:bg-gray-700 header_btn"
+                                                style="color:#000;"
+                                                wire:click="decreaseQuantity('{{ $line->id }}')"
+                                                title="Уменьшить количество">–</button>
+                                            <span>{{ $line->quantity }}</span>
+                                            <button
+                                                class="px-2 py-1  bg-gray-600 rounded hover:bg-gray-700 header_btn"
+                                                style="color:#000;"
+                                                wire:click="increaseQuantity('{{ $line->id }}')"
+                                                title="Увеличить количество">+</button>
+                                        </div>
+                                        <button class="p-2 ml-auto text-gray-600 transition-colors rounded-lg hover:bg-gray-100 hover:text-gray-700" type="button" wire:click="removeLine('{{ $line->id }}')">
+                                            <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"></path>
+                                            </svg>
+                                        </button>
                                     </div>
                                 </div>
                             </div>
