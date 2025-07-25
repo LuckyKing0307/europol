31dd9af717c41d9f8109d899a25a2ba0872e64af
LuckyKing0307 <jasaergasheva@gmail.com>
2025-07-25 15:46:32 +0500

    Update

--- resources/views/components/collection-sale.blade.php
+++ resources/views/components/collection-sale.blade.php
@@ -1,8 +1,8 @@
 <div class="myCatalog">
     <div class="flex max-w-screen-2xl mx-auto justify-between h-full items-center">
         <div class="content-text">
-            <h1 class="title">Европейские полы, которые служат десятилетиями — с гарантией до 25 лет</h1>
-            <a href="{{ route('collection.view.all') }}" class="btn btn-main">Перейти в каталог</a>
+            <h1 class="title" style="width: 100%;">Европейские полы <br> с гарантией до 25 лет</h1>
+            <a href="{{ route('collection.view.all') }}" class="btn btn-main" style="display: inline-block; margin-top: 20px;">Перейти в каталог</a>
         </div>
     </div>
 </div>
