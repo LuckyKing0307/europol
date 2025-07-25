8a101f94332292da31ce68d02333b52625fbae4c
LuckyKing0307 <jasaergasheva@gmail.com>
2025-07-25 15:27:34 +0500

    Update

--- resources/views/components/collection-sale.blade.php
+++ resources/views/components/collection-sale.blade.php
@@ -1,7 +1,7 @@
 <div class="myCatalog">
     <div class="flex max-w-screen-2xl mx-auto justify-between h-full items-center">
         <div class="content-text">
-            <h1 class="title">Полы из Европы, которые служат десятилетиями
+            <h1 class="title" style="width: 50%;">Полы из Европы, которые служат десятилетиями
                 — с гарантией до 25 лет</h1>
             <p class="text">
             -18 лет доверие покупателей. Продаем Европейские полы с 2007 года <br>
