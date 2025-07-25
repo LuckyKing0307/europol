8fd0df1446efb4bddbffcaf0aee7dc12c500856c
LuckyKing0307 <jasaergasheva@gmail.com>
2025-07-25 17:15:28 +0500

    Update

--- resources/views/livewire/components/navigation.blade.php
+++ resources/views/livewire/components/navigation.blade.php
@@ -9,7 +9,6 @@
                     <a href="{{route('maps')}}" class="shops">Магазины</a>
                 </div>
                 <div class="lang flex header-info">
-                    {{--                @livewire('language-switcher')--}}
                     <div class="working-time">с 9:00 до 22:00 ежедневно</div>
                     <a href="tel:+998555100102" class="ml-2"><span><img src="{{asset('img/phone.svg')}}" alt=""
                                                                         style="margin-left: 10px;margin-right: 5px;"></span>+998 55-510-01-02</a>
