69550ff1f382411f4b458b2b972b8d0ba86e7a81
LuckyKing0307 <jasaergasheva@gmail.com>
2025-07-25 12:24:40 +0500

    Update

--- resources/views/components/collection-sale.blade.php
+++ resources/views/components/collection-sale.blade.php
@@ -1,12 +1,9 @@
 <div class="myCatalog">
-    <div class="flex max-w-screen-2xl mx-auto justify-between h-full">
-        <div class="content">
+    <div class="flex max-w-screen-2xl mx-auto justify-between h-full items-center">
+        <div class="content-text">
             <h1 class="title">Напольные решения для вашего интерьера</h1>
             <p class="text">Более 500 вариантов ламината и паркета от ведущих брендов</p>
-            <a href="{{route('collection.view.all')}}" class="btn btn-main">Перейти в каталог</a>
-        </div>
-        <div class="header_img">
-            <img src="{{asset('img/header-bg.svg')}}" alt="">
+            <a href="{{ route('collection.view.all') }}" class="btn btn-main">Перейти в каталог</a>
         </div>
     </div>
 </div>
--- resources/views/components/welcome-banner.blade.php
+++ resources/views/components/welcome-banner.blade.php
@@ -1,5 +1,31 @@
-<section class="bg-gray-50 section" data-aos="fade-in" data-aos-delay="200" data-aos-duration="1000">
+<section class="section pre-loader" data-aos="fade-in" data-aos-delay="200" data-aos-duration="1000" style="background: none;">
+    <div class="video-overlay"></div>
         @if ($this->saleCollection)
             <x-collection-sale />
         @endif
 </section>
+
+
+@push('scripts')
+    <script>
+        document.addEventListener("DOMContentLoaded", function () {
+            const catalog = document.querySelector(".pre-loader");
+
+            const video = document.createElement("video");
+            video.src = "{{ asset('img/pre-view.mp4') }}";
+            video.autoplay = true;
+            video.muted = true;
+            video.loop = true;
+            video.playsInline = true; // для iOS
+            video.classList.add("bg-video");
+
+            catalog.prepend(video);
+            const header = document.querySelector('header');
+
+            if (header && section) {
+                const headerHeight = header.offsetHeight;
+                catalog.style.minHeight = `calc(100vh - ${headerHeight}px)`;
+            }
+        });
+    </script>
+@endpush
