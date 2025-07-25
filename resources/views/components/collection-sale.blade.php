e4cb2e39220cd7b0bf366ae179857a61830f7d07
LuckyKing0307 <jasaergasheva@gmail.com>
2025-07-25 15:40:30 +0500

    Update

--- resources/views/components/collection-sale.blade.php
+++ resources/views/components/collection-sale.blade.php
@@ -1,13 +1,7 @@
 <div class="myCatalog">
-    <div class="flex max-w-screen-2xl mx-auto justify-between h-full items-center">
+    <div class="flex max-w-screen-xl mx-auto justify-between h-full items-center">
         <div class="content-text">
-            <h1 class="title" style="width: 50%;">Европейские полы, которые служат десятилетиями — с гарантией до 25 лет</h1>
-            <p class="text">
-                -18 лет доверия покупателей с 2007 года. <br>
-                -Только оригинальные европейские бренды <br>
-                -В наличии 1000+ вариантов под любой интерьер — от классики до минимализма <br>
-                -Выбор +40 дизайнеров интерьера <br>
-                -Бесплатная доставка по г. Ташкент</p>
+            <h1 class="title">Европейские полы, которые служат десятилетиями — с гарантией до 25 лет</h1>
             <a href="{{ route('collection.view.all') }}" class="btn btn-main">Перейти в каталог</a>
         </div>
     </div>
