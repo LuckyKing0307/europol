68ec0557d01a87e03efd90d42e5b57fcef5dc780
LuckyKing0307 <jasaergasheva@gmail.com>
2025-07-28 08:37:30 +0500

    Update

--- app/Models/CustomProduct.php
+++ app/Models/CustomProduct.php
@@ -12,26 +12,8 @@ class CustomProduct extends LunarProduct
 {
     protected $table = 'products';
 
-    use Searchable;
-    public function getScoutKey(): mixed
-    {
-        return $this->getKey(); // или $this->id
-    }
-    public function getScoutKeyName(): mixed
-    {
-        return 'id'; // или что ты используешь как primary key
-    }
     public function characteristics($limiy = null): HasMany
     {
         return $this->hasMany(ProductCharacteristic::class);
-    }
-    public function toSearchableArray()
-    {
-        return [
-            'id' => $this->id,
-            'name' => $this->translateAttribute('name'), // или $this->name если уже в текущей локали
-            'description' => $this->translateAttribute('description'),
-            // любые другие нужные поля
-        ];
     }
 }
