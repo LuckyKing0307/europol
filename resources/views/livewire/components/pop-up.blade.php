937359441f13e4e1738e0d28e827ea8b00c7d8ad
LuckyKing0307 <jasaergasheva@gmail.com>
2025-07-30 18:11:16 +0500

    Update

--- resources/views/livewire/components/pop-up.blade.php
+++ resources/views/livewire/components/pop-up.blade.php
@@ -1,78 +1,146 @@
 <div class="pop_up-overlay" id="modal" x-data="{ currentStep: 1 }">
     <div class="pop_up-modal">
-        <button class="pop_up-close-btn" @click="document.getElementById('modal').style.display = 'none'">×</button>
-
-        <div class="pop_up-progress-bar">
-            <div class="pop_up-progress-fill" :style="{ width: ((currentStep - 1) / 3 * 100) + '%' }"></div>
+        <div class="pop_up_exit">
+            <p class="pop_up_sub_text">Идеальный пол для любого помещения</p>
+            <button class="pop_up-close-btn" @click="document.getElementById('modal').style.display = 'none'">×</button>
         </div>
-        <h3>Идеальный пол для любого помещения</h3>
-        <p>Ответьте на 4 вопроса и получите идеальный вариант под Вас и скидку 10% на вашу 1 покупку!</p>
-        <div class="pop_up-step" x-show="currentStep === 1" x-cloak>
-            <h3>Вопрос 1:</h3>
-            <p>Как вы о нас узнали?</p>
-            <div class="image-radio-options">
-                <label class="image-radio-option">
-                    <input type="radio" wire:model="q1" name="q1" value="Детская" class="hidden">
-                    <img src="{{asset('popup/detskaya.png')}}" alt="Детская">
-                </label>
+       <div class="pop_up_sub_modal">
+           <p class="pop_up_text">Ответьте на 4 вопроса и получите идеальный вариант под Вас и скидку 10% на вашу 1 покупку!</p>
+           <div class="pop_up-progress-bar">
+               <div class="pop_up-progress-fill" :style="{ width: ((currentStep - 1) / 3 * 100) + '%' }"></div>
+           </div>
+           <div class="pop_up-step" x-show="currentStep === 1" x-cloak>
+               <h3 class="pop_up_text">Для какого помещения нужно напольное покрытие?</h3>
+               <div class="image-radio-options">
 
-                <label class="image-radio-option">
-                    <input type="radio" wire:model="q1" name="q1" value="Кухня" class="hidden">
-                    <img src="{{asset('popup/kitchen.png')}}" alt="Кухня">
-                </label>
+                   <label class="image-radio-option">
+                       <input type="radio" wire:model="q1" name="q1" value="Гостиная" class="hidden">
+                       <img src="{{asset('popup/gostinnaya.png')}}" alt="Гостиная">
+                       <span class="select_pop_up">Гостиная</span>
+                   </label>
+                   <label class="image-radio-option">
+                       <input type="radio" wire:model="q1" name="q1" value="Детская" class="hidden">
+                       <img src="{{asset('popup/detskaya.png')}}" alt="Детская">
+                       <span class="select_pop_up">Детская</span>
+                   </label>
 
-                <label class="image-radio-option">
-                    <input type="radio" wire:model="q1" name="q1" value="Офис" class="hidden">
-                    <img src="{{asset('popup/ofice.png')}}" alt="Офис">
-                </label>
+                   <label class="image-radio-option">
+                       <input type="radio" wire:model="q1" name="q1" value="Коммерческое помещение" class="hidden">
+                       <img src="{{asset('popup/ofice.png')}}" alt="Коммерческое помещение">
+                       <span class="select_pop_up">Коммерческое помещение</span>
+                   </label>
 
-                <label class="image-radio-option">
-                    <input type="radio" wire:model="q1" name="q1" value="Друзья" class="hidden">
-                    <img src="{{asset('popup/gostinnaya.png')}}" alt="Друзья">
-                </label>
-            </div>
-        </div>
+                   <label class="image-radio-option">
+                       <input type="radio" wire:model="q1" name="q1" value="Кухня" class="hidden">
+                       <img src="{{asset('popup/kitchen.png')}}" alt="Кухня">
+                       <span class="select_pop_up">Кухня</span>
+                   </label>
 
-        <div class="pop_up-step" x-show="currentStep === 2" x-cloak>
-            <h3>Вопрос 2:</h3>
-            <p>Что вам важно при выборе?</p>
-            <label class="pop_up-label"><input type="radio" wire:model="q2" name="q2" value="Цена"> Цена</label>
-            <label class="pop_up-label"><input type="radio" wire:model="q2" name="q2" value="Качество"> Качество</label>
-            <label class="pop_up-label"><input type="radio" wire:model="q2" name="q2" value="Скорость доставки"> Скорость доставки</label>
-        </div>
+                   <label class="image-radio-option">
+                       <input type="radio" wire:model="q1" name="q1" value="Спальня" class="hidden">
+                       <img src="{{asset('popup/bedroom.png')}}" alt="Спальня">
+                       <span class="select_pop_up">Спальня</span>
+                   </label>
 
-        <div class="pop_up-step" x-show="currentStep === 3" x-cloak>
-            <h3>Вопрос 3:</h3>
-            <p>Вы бы рекомендовали нас?</p>
-            <label class="pop_up-label"><input type="radio" wire:model="q3" name="q3" value="Да"> Да</label>
-            <label class="pop_up-label"><input type="radio" wire:model="q3" name="q3" value="Нет"> Нет</label>
-            <label class="pop_up-label"><input type="radio" wire:model="q3" name="q3" value="Пока не знаю"> Пока не знаю</label>
-        </div>
+                   <label class="image-radio-option">
+                       <input type="radio" wire:model="q1" name="q1" value="Прихожая" class="hidden">
+                       <img src="{{asset('popup/Прихож.png')}}" alt="Прихожая">
+                       <span class="select_pop_up">Прихожая</span>
+                   </label>
+               </div>
+           </div>
 
-        <div class="pop_up-step" x-show="currentStep === 4" x-cloak>
-            <h3>Ваш номер:</h3>
-            <input type="text" placeholder="+998901234567" wire:model="phone" class="pop_up-input">
-            <h3>Ваше Имя:</h3>
-            <input type="text" placeholder="Алишер Авазов" wire:model="name" class="pop_up-input">
-        </div>
+           <div class="pop_up-step" x-show="currentStep === 2" x-cloak>
+               <h3 class="pop_up_text">Выберите цвет напольного покрытия</h3>
+               <div class="image-radio-options">
+                   <label class="image-radio-option">
+                       <input type="radio" wire:model="q2" name="q2" value="Белый" class="hidden">
+                       <img src="{{asset('popup/Beliy.png')}}" alt="Белый">
+                       <span class="select_pop_up">Белый</span>
+                   </label>
+                   <label class="image-radio-option">
+                       <input type="radio" wire:model="q2" name="q2" value="Натуральный" class="hidden">
+                       <img src="{{asset('popup/Naturalniy.png')}}" alt="Натуральный">
+                       <span class="select_pop_up">Натуральный</span>
+                   </label>
 
-        <div class="pop_up-buttons mt-4">
-            <button class="pop_up-button" type="button" @click="if(currentStep > 1) currentStep--" x-bind:disabled="currentStep === 1">
-                Назад
-            </button>
+                   <label class="image-radio-option">
+                       <input type="radio" wire:model="q2" name="q2" value="Красный" class="hidden">
+                       <img src="{{asset('popup/krasniy.png')}}" alt="Красный">
+                       <span class="select_pop_up">Красный</span>
+                   </label>
 
-            <template x-if="currentStep < 4">
-                <button class="pop_up-button" type="button" @click="if (validateStep(currentStep)) currentStep++">
-                    Далее
-                </button>
-            </template>
+                   <label class="image-radio-option">
+                       <input type="radio" wire:model="q2" name="q2" value="Коричневый" class="hidden">
+                       <img src="{{asset('popup/brown.png')}}" alt="Коричневый">
+                       <span class="select_pop_up">Коричневый</span>
+                   </label>
 
-            <template x-if="currentStep === 4">
-                <button class="pop_up-button" type="button" wire:click="submitData">
-                    Отправить
-                </button>
-            </template>
-        </div>
+                   <label class="image-radio-option">
+                       <input type="radio" wire:model="q2" name="q2" value="Серый" class="hidden">
+                       <img src="{{asset('popup/gray.png')}}" alt="Серый">
+                       <span class="select_pop_up">Серый</span>
+                   </label>
+
+                   <label class="image-radio-option">
+                       <input type="radio" wire:model="q2" name="q2" value="Черный" class="hidden">
+                       <img src="{{asset('popup/black.png')}}" alt="Черный">
+                       <span class="select_pop_up">Черный</span>
+                   </label>
+               </div>
+           </div>
+
+           <div class="pop_up-step" x-show="currentStep === 3" x-cloak>
+               <h3>Вопрос 3:</h3>
+               <div class="image-radio-options">
+                   <label class="image-radio-option">
+                       <input type="radio" wire:model="q3" name="q3" value="Паркет" class="hidden">
+                       <img src="{{asset('popup/Parket.png')}}" alt="Паркет">
+                       <span class="select_pop_up">Паркет</span>
+                   </label>
+                   <label class="image-radio-option">
+                       <input type="radio" wire:model="q3" name="q3" value="Ламинат" class="hidden">
+                       <img src="{{asset('popup/Laminate.png')}}" alt="Ламинат">
+                       <span class="select_pop_up">Ламинат</span>
+                   </label>
+
+                   <label class="image-radio-option">
+                       <input type="radio" wire:model="q3" name="q3" value="ПВХ/SPC" class="hidden">
+                       <img src="{{asset('popup/pvx.png')}}" alt="ПВХ/SPC">
+                       <span class="select_pop_up">ПВХ/SPC</span>
+                   </label>
+
+                   <label class="image-radio-option">
+                       <input type="radio" wire:model="q3" name="q3" value="Ковролин" class="hidden">
+                       <img src="{{asset('popup/kovrolin.png')}}" alt="Ковролин">
+                       <span class="select_pop_up">Ковролин</span>
+                   </label>
+               </div>
+           </div>
+
+           <div class="pop_up-step" x-show="currentStep === 4" x-cloak>
+               <input type="text" placeholder="Телефон" wire:model="phone" class="pop_up-input">
+               <input type="text" placeholder="Имя" wire:model="name" class="pop_up-input">
+           </div>
+
+           <div class="pop_up-buttons mt-4">
+               <button class="pop_up-button" type="button" @click="if(currentStep > 1) currentStep--" x-bind:disabled="currentStep === 1">
+                   Назад
+               </button>
+
+               <template x-if="currentStep < 4">
+                   <button class="pop_up-button" type="button" @click="if (validateStep(currentStep)) currentStep++">
+                       Далее
+                   </button>
+               </template>
+
+               <template x-if="currentStep === 4">
+                   <button class="pop_up-button" type="button" wire:click="submitData">
+                       Отправить
+                   </button>
+               </template>
+           </div>
+       </div>
     </div>
 </div>
 
