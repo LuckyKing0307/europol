3823ef4f24116c4c2e105ff3438584c08c3aad24
LuckyKing0307 <jasaergasheva@gmail.com>
2025-08-08 17:50:56 +0500

    Update

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,6 +3,7 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Sentry\Laravel\Integration;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -23,8 +24,9 @@ class Handler extends ExceptionHandler
      */
     public function register(): void
     {
+        Integration::handles($this);
         $this->reportable(function (Throwable $e) {
-            //
+            return response()->json(['message' => $e->getMessage()], 400);
         });
     }
 }
