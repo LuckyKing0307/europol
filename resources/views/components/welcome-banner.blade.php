450c88d9ed283e7f7a4850f5c8df6dd57820233e
LuckyKing0307 <jasaergasheva@gmail.com>
2025-08-14 23:22:46 +0500

    Update

--- resources/views/components/welcome-banner.blade.php
+++ resources/views/components/welcome-banner.blade.php
@@ -6,24 +6,22 @@
 
 @push('scripts')
     <script>
-        document.addEventListener("DOMContentLoaded", function () {
-            const catalog = document.querySelector(".pre-loader");
+        const catalog = document.querySelector(".pre-loader");
 
-            const video = document.createElement("video");
-            video.src = "{{ asset('img/pre-view.mp4') }}";
-            video.autoplay = true;
-            video.muted = true;
-            video.loop = true;
-            video.playsInline = true; // для iOS
-            video.classList.add("bg-video");
+        const video = document.createElement("video");
+        video.src = "{{ asset('img/pre-view.mp4') }}";
+        video.autoplay = true;
+        video.muted = true;
+        video.loop = true;
+        video.playsInline = true; // для iOS
+        video.classList.add("bg-video");
 
-            catalog.prepend(video);
-            const header = document.querySelector('header');
+        catalog.prepend(video);
+        const header = document.querySelector('header');
 
-            if (header && section) {
-                const headerHeight = header.offsetHeight;
-                catalog.style.minHeight = `calc(100vh - ${headerHeight}px)`;
-            }
-        });
+        if (header) {
+            const headerHeight = header.offsetHeight;
+            catalog.style.minHeight = `calc(100vh - ${headerHeight}px)`;
+        }
     </script>
 @endpush
