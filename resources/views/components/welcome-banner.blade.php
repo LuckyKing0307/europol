f5ec61d3cf1795378d06ff91ea81cb6b2e322d9c
LuckyKing0307 <jasaergasheva@gmail.com>
2025-08-15 00:21:08 +0500

    Update

--- resources/views/components/welcome-banner.blade.php
+++ resources/views/components/welcome-banner.blade.php
@@ -1,54 +1,6 @@
 <section class="section pre-loader" data-aos="fade-in" data-aos-delay="200" data-aos-duration="1000" style="background: none;">
-    <div class="video-overlay"></div>
+    <video class="bg-video" autoplay muted loop playsinline preload="auto" poster="{{ asset('img/pre-view.webp') }}">
+        <source src="{{ asset('img/pre-view.mp4') }}" type="video/mp4">
+    </video>
     <x-collection-sale />
 </section>
-
-@push('scripts')
-    <script>
-        document.addEventListener("DOMContentLoaded", () => {
-            const catalog = document.querySelector(".pre-loader");
-            if (!catalog) return;
-
-            const video = document.createElement("video");
-            video.poster = "{{ asset('img/pre-view.webp') }}"; // моментально загружаемый постер
-            video.autoplay = true;
-            video.muted = true;
-            video.loop = true;
-            video.playsInline = true;
-            video.preload = "none"; // не загружаем видео до вызова
-            video.classList.add("bg-video");
-
-            catalog.prepend(video);
-
-            // подгон высоты
-            const header = document.querySelector('header');
-            if (header) {
-                const headerHeight = header.offsetHeight;
-                catalog.style.minHeight = `calc(100vh - ${headerHeight}px)`;
-            }
-
-            // Ленивая загрузка видео
-            const observer = new IntersectionObserver((entries) => {
-                if (entries[0].isIntersecting) {
-                    // создаём источники для webm и mp4
-                    const sourceWebm = document.createElement("source");
-                    sourceWebm.src = "{{ asset('img/pre-view.webp') }}";
-                    sourceWebm.type = "video/webm";
-
-                    const sourceMp4 = document.createElement("source");
-                    sourceMp4.src = "{{ asset('img/pre-view-test.mp4') }}";
-                    sourceMp4.type = "video/mp4";
-
-                    video.appendChild(sourceWebm);
-                    video.appendChild(sourceMp4);
-
-                    video.load();
-                    video.play();
-                    observer.disconnect();
-                }
-            }, { rootMargin: "200px" }); // начинаем подгружать чуть раньше
-
-            observer.observe(catalog);
-        });
-    </script>
-@endpush
