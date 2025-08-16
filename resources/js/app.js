e7a4dfacc31b0c0ebbb7f3eb2ab4838edb66d401
LuckyKing0307 <jasaergasheva@gmail.com>
2025-08-16 20:18:49 +0500

    Update

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,3 @@
-// resources/js/app.js
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
@@ -7,19 +6,7 @@ import 'nouislider/dist/nouislider.css';
 import Swiper from 'swiper/bundle';
 import noUiSlider from 'nouislider';
 import BeerSlider from 'beerslider';
-import Pusher from 'pusher-js';
-import Echo from 'laravel-echo';
 
 window.Swiper = Swiper;
 window.noUiSlider = noUiSlider;
 window.BeerSlider = BeerSlider;
-
-window.Pusher = Pusher;
-
-window.Echo = new Echo({
-    broadcaster: 'pusher',
-    key: import.meta.env.VITE_PUSHER_APP_KEY,
-    cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
-    forceTLS: true,
-});
-
